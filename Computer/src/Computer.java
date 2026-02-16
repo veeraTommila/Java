60e8964fe679d8a3603961d2674d6740a76e3d0c
Veera Tommila <veera.tommila@gmail.com>
2026-02-16 20:54:09 +0200

    Create Computer.java
    I created a Programming_Excersises project and added a Computer class to it. In the class, I created variables memoryAmount and processorModel, which I set to the values ​​2048 and Intel, and print methods to print texts. First, the text is printed: Hello from public Java class named Computer! and then the amount of memory and the processor model.

---
+++ Computer/src/Computer.java
@@ -0,0 +1,14 @@
+
+public class Computer {
+	
+
+	public static void main(String[] args) {
+		int memoryAmount = 2048;
+		String processorModel = "Intel";
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello from public Java class named Computer!");	//Prints the text Hello from public Java class named Computer!	
+		System.out.println("The memory amount: " + memoryAmount + ". The processor model: " + processorModel + ".");	
+	}
+
+}
